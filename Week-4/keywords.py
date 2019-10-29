a7c7434d72a4b8240ff6bd20cf66fad71d732e8c
01763771 <56557515+01763771@users.noreply.github.com>
2019-10-29 10:17:16 +0000

    keywords

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -4,14 +4,16 @@ keywords.py
 Create functions that use keyword arguments
 with default values.
 '''
-def welcome_message(username, place):
-    username = input()
-    place = input()
-    if username = 6:
-        print(ght)
+
+def welcome_message(user_name = None, place = None):
+    if (user_name == None) and (place == None):
+        return 'Hello and welcome'
+    elif (user_name != None) and (place == None):
+        return ('Hello, ' + user_name + ', and welcome')
+    elif (user_name == None) and (place != None):
+        return ('Hello and welcome to ' + place)
     else:
-        print(hel)
-        
+        return ('Hello, ' + user_name + ', and welcome to ' + place)    
 # Create a function called welcome_message():
 # if no input argument is provided
 # it returns the string 'Hello and welcome'
@@ -22,7 +24,20 @@ def welcome_message(username, place):
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
 
-def list_average(list, avg_type)
+def list_average(num_list, avg_type = None):
+    if num_list == []:
+        return 0
+    if avg_type == None or avg_type == 'mean':
+        return sum(num_list)/len(num_list)
+    elif avg_type == 'median':
+        s = num_list.sort()
+        n = len(num_list)
+        return ((sum(s[n//2-1:n//2+1])/ 2.0, s[n//2])[n % 2])
+        
+
+
+
+
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
