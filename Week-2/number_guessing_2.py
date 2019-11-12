8be7dd12e9736042422e5486d9341b0b9625e048
01763771 <56557515+01763771@users.noreply.github.com>
2019-11-12 08:17:53 +0000

    practice

--- Week-2/number_guessing_2.py
+++ Week-2/number_guessing_2.py
@@ -1,1 +1,11 @@
-my_input = input('Guess a number between 1 and 10: ')
+def number_guessing_game():
+    '''Number guessing game.'''
+    import random
+    input_1 = int(input('Guess a number between 1 and 10: '))
+    random_1 = random.randint(1, 10)
+    while input_1 != random_1:
+        import random
+        print('False')
+        input_1 = int(input('Guess a number between 1 and 10: '))
+        random_1 = random.randint(1, 10)
+    print('True')
